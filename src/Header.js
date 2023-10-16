d965ba1e6f5048df8bb319f14890113d111c63aa
Robert Garcia <robertserranogar@gmail.com>
2023-10-15 18:47:34 -0600

    commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header-wrap">
+            <p>Social Media App</p>
+        </header>
+    );
+};
+
+export default Header;
