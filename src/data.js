cdc7e8e0cfebc5913f3e02c7e513f2ff54961e35
Robert Garcia <robertserranogar@gmail.com>
2023-10-15 18:45:50 -0600

    commit

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+      id: 1,
+      userName: "leomessi",
+      postLiked: false,
+      comments: [],
+      img: "img/leomessi.png"
+    },
+    {
+      id: 2,
+      userName: "apple",
+      postLiked: false,
+      comments: [],
+      img: "img/apple.png"
+    },
+    {
+      id: 3,
+      userName: "theweeknd",
+      postLiked: false,
+      comments: [],
+      img: "img/theweeknd.png"
+    },
+    {
+      id: 4,
+      userName: "toddhido",
+      postLiked: false,
+      comments: [],
+      img: "img/toddhido.png"
+    },
+    {
+      id: 5,
+      userName: "ronaldo",
+      postLiked: false,
+      comments: [],
+      img: "img/ronaldo.png"
+    },
+    {
+      id: 6,
+      userName: "lewishamilton",
+      postLiked: false,
+      comments: [],
+      img: "img/lewis.png"
+    },
+    {
+      id: 7,
+      userName: "vogue",
+      postLiked: false,
+      comments: [],
+      img: "img/vogue.png"
+    }
+  ];
+  
+  export default data;
+  
